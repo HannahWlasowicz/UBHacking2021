ec9a488f0fadd5dbb977cfa2de75b34c6f19b399
Hannah <hannahwl@buffalo.edu>
2021-11-06 18:30:31 -0400

    dropdown now has all states

--- modules/main.js
+++ modules/main.js
@@ -1,10 +1,82 @@
 import {loadLine} from './line.js'
 import {loadMap} from './map.js'
 function loadData(){
-    console.log("Poggers");
+    dropdown();
+    console.log('Poggers');
     // loadMap();
     loadLine();
 }
-console.log("Tester");
-document.getElementById("body").onload = function() {loadData()};
+console.log('Tester');
+document.getElementById('body').onload = function() {loadData()};
 // loadData();
+
+
+ function dropdown() {
+    // document.getElementById('selection')
+    var values = [
+        ['United States', 'USA'],
+        ['Arizona', 'AZ'],
+        ['Alabama', 'AL'],
+        ['Alaska', 'AK'],
+        ['Arkansas', 'AR'],
+        ['California', 'CA'],
+        ['Colorado', 'CO'],
+        ['Connecticut', 'CT'],
+        ['Delaware', 'DE'],
+        ['Florida', 'FL'],
+        ['Georgia', 'GA'],
+        ['Hawaii', 'HI'],
+        ['Idaho', 'ID'],
+        ['Illinois', 'IL'],
+        ['Indiana', 'IN'],
+        ['Iowa', 'IA'],
+        ['Kansas', 'KS'],
+        ['Kentucky', 'KY'],
+        ['Louisiana', 'LA'],
+        ['Maine', 'ME'],
+        ['Maryland', 'MD'],
+        ['Massachusetts', 'MA'],
+        ['Michigan', 'MI'],
+        ['Minnesota', 'MN'],
+        ['Mississippi', 'MS'],
+        ['Missouri', 'MO'],
+        ['Montana', 'MT'],
+        ['Nebraska', 'NE'],
+        ['Nevada', 'NV'],
+        ['New Hampshire', 'NH'],
+        ['New Jersey', 'NJ'],
+        ['New Mexico', 'NM'],
+        ['New York', 'NY'],
+        ['North Carolina', 'NC'],
+        ['North Dakota', 'ND'],
+        ['Ohio', 'OH'],
+        ['Oklahoma', 'OK'],
+        ['Oregon', 'OR'],
+        ['Pennsylvania', 'PA'],
+        ['Rhode Island', 'RI'],
+        ['South Carolina', 'SC'],
+        ['South Dakota', 'SD'],
+        ['Tennessee', 'TN'],
+        ['Texas', 'TX'],
+        ['Utah', 'UT'],
+        ['Vermont', 'VT'],
+        ['Virginia', 'VA'],
+        ['Washington', 'WA'],
+        ['West Virginia', 'WV'],
+        ['Wisconsin', 'WI'],
+        ['Wyoming', 'WY'],
+    ];
+ 
+    var select = document.createElement('select');
+    select.name = 'states';
+    select.id = 'states'
+ 
+    for (var val of values)
+    {
+        var option = document.createElement('option');
+        option.value = val[1];
+        option.text = val[0];
+        select.appendChild(option);
+    } 
+    document.getElementById('selection').appendChild(select);
+}
